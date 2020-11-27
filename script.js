2a53e559c3a550ddd4b7d3e17679b085313a9de9
Michelle Racho <rachoma12@gmail.com>
2020-11-27 17:51:00 -0500

    email validation working ~90% - still has bug

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
-const firstName = document.querySelector('#first-name');
-const lastName = document.querySelector('#last-name');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
 const submitBtn = document.querySelector('#submit-btn');
-
 const form = document.querySelector('.signup-form');
-
-// const inputs = document.querySelectorAll('input');
 const inputs = document.querySelectorAll('.signup-form__input-container');
-// const inputSpan = document.querySelectorAll('input-span');
 
-// const error = document.querySelectorAll('error');
-// const errorMsg = document.querySelectorAll('error-msg');
-// const errorIcon = document.querySelectorAll('error-icon');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#email + .error');
+const emailErrorSpan = document.querySelector('#email + .error > .error__msg');
 
-const validateEmail = email => {
+const emailPattern = email => {
   // abc@email.com
   const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
   return pattern.test(email.toLowerCase());
 };
 
-const checkInput = () => {
-  const isValidEmail = validateEmail(email.value);
-  const emailError = document
-    .querySelector('#email')
-    .nextElementSibling.querySelector('.error__msg');
+const validateEmail = () => {
+  const isValidEmail = emailPattern(email.value);
 
+  if (email.value !== '' && !isValidEmail) {
+    emailError.classList.add('show');
+    emailErrorSpan.innerHTML = 'Looks like this is not an email';
+    email.classList.add('error-input');
+  }
+};
+
+const checkInput = () => {
   for (let i = 0; i <= inputs.length - 1; i++) {
     let error = inputs[i].querySelector('.error');
     let inputValue = inputs[i].querySelector('.signup-form__input');
     let inputSpan = inputs[i].querySelector('.input-span');
+    // let errorMsg = inputs[i].querySelectorAll('.error-msg');
 
     // check input value
     if (inputValue.value === '') {
       error.classList.add('show');
       inputValue.classList.add('error-input');
       inputSpan.innerText = inputValue.placeholder;
+      // errorMsg.innerHTML = `${inputValue.placeholder} cannot be empty`;
     }
-    // remove error
-    else {
-      // validate email
-      if (email.value !== '' && !isValidEmail) {
-        // if (!isValidEmail) {
-        inputValue.classList.add('error-input');
-        inputValue.nextElementSibling.classList.add('show');
-        // emailError.innerHTML = 'Looks like this is not an email';
-        console.log(isValidEmail, email.value);
-      }
 
+    // validate email and remove error
+    else {
       error.classList.remove('show');
       inputValue.classList.remove('error-input');
+
+      validateEmail();
     }
   }
 };
